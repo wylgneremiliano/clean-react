6673ed7740e6f1fd669776471fb15faec9d4d0dd
Wylgner Emiliano <wylgneremiliano@gmail.com>
2021-03-01 18:04:31 -0300

    test: ensure RemoteAuthentication calls HttpPostClient with correct URL

--- src/domain/usecases/authenticate.ts
+++ src/domain/usecases/authenticate.ts
@@ -1,9 +1,9 @@
-import { AccountModel } from '../models/account-model';
+import { AccountModel } from '../models/account-model'
 
 type AuthenticationParams = {
-  email: string;
-  password: string;
-};
+  email: string
+  password: string
+}
 export interface Authentication {
-  auth(params: AuthenticationParams): Promise<AccountModel>;
+  auth(params: AuthenticationParams): Promise<AccountModel>
 }
