86b0a62ef200e491c82f12718386a907b7726fb1
Wylgner Emiliano <wylgneremiliano@gmail.com>
2021-03-01 18:10:26 -0300

    refactor: move HttpPostClient Spy to its own file

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,17 +1,8 @@
+import { HttpPostClientSpy } from '../../test/mock-http.client'
 import { RemoteAuthentication } from './remote-authentication'
-import { HttpPostClient } from 'data/protocols/http/http-post-client'
 
 describe('RemoteAuthentication', () => {
   test('Should call HttpPostClient with correct URL', async () => {
-    class HttpPostClientSpy implements HttpPostClient {
-      url?: string
-
-      async post(url: string): Promise<void> {
-        this.url = url
-        return Promise.resolve()
-      }
-    }
-
     const url = 'any_url'
     const httpPostClientSpy = new HttpPostClientSpy()
     const sut = new RemoteAuthentication(url, httpPostClientSpy)
