494f3d41b15d9858dcbd32102179e82f0bf17b46
Wylgner Emiliano <wylgneremiliano@gmail.com>
2021-03-02 15:44:49 -0300

    test: ensure RemoteAutentication returns an Account on success

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,6 +1,9 @@
 import { HttpPostClientSpy } from '@/data/test/mock-http.client'
 import { RemoteAuthentication } from './remote-authentication'
-import { mockAuthentication } from '@/domain/test/mock-authentication'
+import {
+  mockAccountModel,
+  mockAuthentication,
+} from '@/domain/test/mock-account'
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
 import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { UnexpectedError } from '@/domain/errors/unexpected-error'
@@ -54,7 +57,6 @@ describe('RemoteAuthentication', () => {
     const promice = sut.auth(mockAuthentication())
     await expect(promice).rejects.toThrow(new UnexpectedError())
   })
-
   test('Should trhrow UnexpectedErro if HttpPostClient returns 500', async () => {
     const { sut, httpPostClientSpy } = makeSut()
     httpPostClientSpy.response = {
@@ -63,12 +65,22 @@ describe('RemoteAuthentication', () => {
     const promice = sut.auth(mockAuthentication())
     await expect(promice).rejects.toThrow(new UnexpectedError())
   })
-  test('Should trhrow UnexpectedErro if HttpPostClient returns 404', async () => {
+  test('Should throw UnexpectedErro if HttpPostClient returns 404', async () => {
     const { sut, httpPostClientSpy } = makeSut()
     httpPostClientSpy.response = {
       statusCode: HttpStatusCode.notFound,
     }
     const promice = sut.auth(mockAuthentication())
     await expect(promice).rejects.toThrow(new UnexpectedError())
+  })
+  test('Should return an AccountModel if HttpPostClient returns 200', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    const httpResult = mockAccountModel()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: httpResult,
+    }
+    const account = await sut.auth(mockAuthentication())
+    await expect(account).toEqual(httpResult)
   })
 })
